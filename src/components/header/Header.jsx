4e85992cbebc301e550c016d4794602680b12bba
anamuddinahmad <anamuddinahmad0786@gmail.com>
2024-12-22 22:26:55 +0530

    Cofiguring Store

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import { LogoutBtn, Container, Logo } from "../index";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
-
-export default Header
+  const authStatus = useSelector((state) => state.auth.status);
+  return <div>Header</div>;
+};
+ 
+export default Header;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./header/LogoutBtn";
 
-export { Header, Footer };
+export { Header, Footer ,Logo ,Container ,LogoutBtn };
