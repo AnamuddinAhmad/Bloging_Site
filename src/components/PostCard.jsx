a40782b983a300140701096c967cefcfda452b3c
anamuddinahmad <anamuddinahmad0786@gmail.com>
2024-12-25 00:20:33 +0530

    Setup Select and PostCard is going On

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import appwriteService from "../appwrite/consf";
+import {Link} from "react-router-dom";
+
+export const PostCard = ({$id,title,featuredImage}) => {
+  return (
+    <div>PostCard</div>
+  )
+}
