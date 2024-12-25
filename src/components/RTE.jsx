eefe15b4eb577572ef17c84e62092ccde2b19767
anamuddinahmad <anamuddinahmad0786@gmail.com>
2024-12-26 00:26:04 +0530

    Real Time Editor Done

---
+++ src/components/RTE.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { Controller } from "react-hook-form";
+
+const RTE = ({ name, control, label, defaultValue = "" }) => {
+  return (
+    <div className="w-full">
+      {label && <label className="inline-block mb-1 pl-1">{label}</label>}
+      <Controller
+        name={name || "content"}
+        control={control}
+        render={({ field: { onChange } }) => (
+          <Editor
+            initialValue={defaultValue}
+            init={{
+              branding: false,
+              height: 500,
+              menubar: true,
+              plugins: [
+                "images",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insetdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor",
+              ],
+              toolbar:
+                "undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+            }}
+            onEditorChange={onChange}
+          />
+        )}
+      />
+    </div>
+  );
+};
+
+export default RTE;
